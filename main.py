f47925f2ca3290ff625620f890dafa20f2ee77c4
Marcela Melara <mramosmelara @gmail.com>
2023-11-13 00:28:09 -0600

    Incluye CORS

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 from mailjet import send_email
 import os
 
 app = Flask(__name__)
-
+CORS(app)
 
 @app.route('/')
 def index():
